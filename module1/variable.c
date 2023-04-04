1e0bcd6238f3b3c241ed06e2a5475bc1315e6c71
im-azad <me.azad99@gmail.com>
2023-04-04 19:56:34 +0600

    add moudle1 and editor setting.

---
+++ module1/variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    /* code */
+    int a, b, sum;
+    printf("Enter value of a ");
+    scanf("%d", &a);
+    printf("Enter value of b ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("The sum of a and b %d", sum);
+
+    return 0;
+}

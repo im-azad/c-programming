ae4c86b13b87c89ff3ba83ebc76c4091eb513625
im-azad <me.azad99@gmail.com>
2023-04-04 20:09:35 +0600

    condition check using (is, else)

---
+++ module2/condition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    if (5 > 4)
+    {
+        printf("condition is true 5 is getter then 4 \n");
+    }
+    else
+    {
+        printf("condition is False");
+    }
+    return 0;
+}

5f8b0fdee5ed4a8a6aca2379bf18cdad7f377780
im-azad <me.azad99@gmail.com>
2023-04-06 23:45:36 +0600

    code push

---
+++ module3/quize.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+    // print 1 to 5000 digit summations;
+
+   int a = 0, i;
+    for (i = 0;i < 5; i++)
+    {
+        continue;
+        a++;
+    }
+    printf("%d %d",i,a);
+    return 0;
+}
---
+++ practice2/loop.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, i;
+
+    scanf("%d", &n);
+    if (n > 0)
+    {
+        for ( i = 1; i <= n; i++)
+        {
+          
+            printf("I Love Practice\n");
+        }
+        
+    }
+    
+
+    return 0;
+}

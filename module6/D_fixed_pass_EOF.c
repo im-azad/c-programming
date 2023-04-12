2d4703f084b37a89e0f6d1c8d06d43afaea3587c
im-azad <me.azad99@gmail.com>
2023-04-12 20:20:18 +0600

    moudle 6 problem soleve

---
+++ module6/D_fixed_pass_EOF.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    while (scanf("%d", &n) != EOF) // end of file
+    {
+        if (n == 1999)
+        {
+            printf("Correct\n");
+            break;
+        }
+        else
+        {
+            printf("Wrong\n");
+        }
+    }
+
+    return 0;
+}
